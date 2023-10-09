00187b07b01975cc04a78e55ae512ff84ab2ef0a
EC2 Default User <ec2-user@ip-172-31-35-128.ap-northeast-1.compute.internal>
2023-10-09 13:06:50 +0000

    [Finish]bookのdestroy機能の作

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'lists/new'
   get 'top' => 'homes#top'
-  resources :lists
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
